7b2d3a31e1a2cff57a91ae3e8fa7e6bd6a130dd4
Abdykariev Emir <emir22052004@gmail.com>
2026-01-24 18:45:54 +0600

    Добавлены функции удаления, изменения названия и получения всех книг

--- homework_7.py
+++ homework_7.py
@@ -22,8 +22,29 @@ def insert_book(connection, name, author, year, genre, pages, copies):
     """, (name, author, year, genre, pages, copies))
     connection.commit()
 
+def get_all_books(connection):
+    result = connection.execute("SELECT * FROM books")
+    return result.fetchall()
+
+
+def update_book_name(connection, book_id, new_name):
+    connection.execute(
+        "UPDATE books SET name = ? WHERE book_id = ?",
+        (new_name, book_id)
+    )
+    connection.commit()
+
+
+def delete_book(connection, book_id):
+    connection.execute(
+        "DELETE FROM books WHERE book_id = ?",
+        (book_id,)
+    )
+    connection.commit()
+
 if __name__ == "__main__":
     conn = sqlite3.connect("library.db")
+
     create_table(conn)
 
     insert_book(conn, "Sapiens", "Юваль Ной Харари", 2011, "История", 512, 5)
@@ -36,3 +57,14 @@ if __name__ == "__main__":
     insert_book(conn, "Логика", "Аристотель", -330, "Философия", 300, 2)
     insert_book(conn, "Моя борьба", "Адольф Гитлер", 1925, "Политика", 720, 1)
     insert_book(conn, "Преступный человек", "Чезаре Ломброзо", 1876, "Криминология", 560, 2)
+
+    print("Все книги:")
+    for book in get_all_books(conn):
+        print(book)
+
+    update_book_name(conn, 1, "Sapiens: Краткая история человечества")
+    delete_book(conn, 10)
+
+    print("\nПосле изменений:")
+    for book in get_all_books(conn):
+        print(book)
